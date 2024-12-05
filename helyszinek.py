1e4afae1436f8109dd3a42785632c097d4d051cb
nvkmln <novak.milan@students.jedlik.eu>
2024-12-05 11:19:59 +0100

    Beertel Gyorbe

--- helyszinek.py
+++ helyszinek.py
@@ -1,12 +1,11 @@
 import random
 import os
 def gyomore():
-    print("Ahhoz, hogy eljuss Becsbe, eloszor el kell menj otthonrol Gyorbe, ezert Gyomoren kezdesz.\n")
-    v = input("Szerinted kesik a vonatod?\n1 - Igen\n2 - Nem\n")
-    os.system("cls")
+    print("\nAhhoz, hogy eljuss Becsbe, eloszor el kell menj otthonrol Gyorbe, ezert Gyomoren kezdesz.\n")
+
 def gyor():
     os.system("cls")
-    print("Helyszin: Gyor, Vasutallomas\n1 - Iskola(Lyedlick)\n2 - Dohanybolt\n3 - Arkad\n4 - Baross ut\n5 - Becsi vonat")
+    print("\nHelyszin: Gyor, Vasutallomas\n1 - Iskola(Lyedlick)\n2 - Dohanybolt\n3 - Arkad\n4 - Baross ut\n5 - Becsi vonat")
 def iskola():
     os.system("cls")
-    print("Biztos be akarsz menni iskolaba? (-40 energia)")
+    print("\nBiztos be akarsz menni iskolaba? (-40 energia)")
--- main.py
+++ main.py
@@ -30,12 +30,37 @@ def main():
     global hely
     match v:
         case "1":
-            energia = 100
-            penz = 2500
-            os.system("cls")
-            print("Udv a Gyor Simulator 2-ben, ahol ugyanaz a celod, mint az elso kiadasban, hogy megvedd a csodas vonatjegyed Becsbe. Sok szerencset!")
-            input("\nENTER")
-            os.system("cls")
-            
+                energia = 100
+                penz = 2500
+                os.system("cls")
+                print("Udv a Gyor Simulator 2-ben, ahol ugyanaz a celod, mint az elso kiadasban, hogy megvedd a csodas vonatjegyed Becsbe. Sok szerencset!")
+                input("\nENTER")
+                os.system("cls")
+                energia = 100
+                penz = 2500 #eleg egy zacsko dohanyra
+                def game():
+                    global energia
+                    global penz
+                    global hely
+                    print(f'Energia: {energia}%', end='\t\t')
+                    print(f'Egyenleg: {penz}Ft', end='\t\t')
+                    hely = gyomore()
+                    v = input("\nSzerinted kesik a vonatod?\n1 - Igen\n2 - Nem\n")
+                    os.system("cls")
+                    if v == "1" or v == "2":
+                        print(f'Energia: {energia}%', end='\t\t')
+                        print(f'Egyenleg: {penz}Ft', end='\t\t')
+                        v= input(f"\nSajnos a vonatod {random.randint(1,121)} percet kesik.\nMegvarod?\n1 - Igen\n2 - Nem\n")
+                        if v == "1":
+                            pass
+                        elif v == "2":
+                            os.system("cls")
+                            energia = 0
+                            penz = 0
+                            vesztettel()
+                    hely = gyor()
+                    v = input("Hova szeretnel menni?")
+                game()
+
 
 main()
