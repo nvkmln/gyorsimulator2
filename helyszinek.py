41bbe1e565e9e952e3ccebe5d8c597ec75f5e22c
nvkmln <novak.milan@students.jedlik.eu>
2024-12-05 10:48:37 +0100

    Alap dolgokat beleraktam csak

---
+++ helyszinek.py
@@ -0,0 +1,12 @@
+import random
+import os
+def gyomore():
+    print("Ahhoz, hogy eljuss Becsbe, eloszor el kell menj otthonrol Gyorbe, ezert Gyomoren kezdesz.\n")
+    v = input("Szerinted kesik a vonatod?\n1 - Igen\n2 - Nem\n")
+    os.system("cls")
+def gyor():
+    os.system("cls")
+    print("Helyszin: Gyor, Vasutallomas\n1 - Iskola(Lyedlick)\n2 - Dohanybolt\n3 - Arkad\n4 - Baross ut\n5 - Becsi vonat")
+def iskola():
+    os.system("cls")
+    print("Biztos be akarsz menni iskolaba? (-40 energia)")
