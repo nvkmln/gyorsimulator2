573a491851a9d429c7f8cd70e55b21cee4464e99
nvkmln <novak.milan@students.jedlik.eu>
2024-12-09 21:17:44 +0100

    fuggvenyekben dolgozva mukodik a cucclii

--- Zsomby/helyek.py
+++ Zsomby/helyek.py
@@ -1,19 +1,119 @@
 import random, os
 
+energia = 100
+penz = 2500
+ora = 6
+perc = 42
+
+def ido(ora: int, perc: int):
+    ido = f"{ora}:{perc}"
+    if perc > 60:
+        ora += 1
+        perc = perc % 60
+        ido = f"{ora}:{perc}"
+    elif perc == 60:
+        ora += 1
+        perc = 0
+        ido = f"{ora}:{perc}0"
+    elif perc > 0 and perc < 10:
+        ido = f"{ora}:0{perc}"
+    elif perc == 0:
+        ido = f"{ora}:0{perc}"
+    return print(ido)
+
+def statPrinteles(energia: int, penz: int):
+    print(f"Energia:{energia}\t\tPenz: {penz}Ft", end="\t\t")
+
+
 def gyomore(v: str):
+    global energia
+    global penz
+    global ora
+    global perc
+    os.system("cls")
+    statPrinteles(energia, penz)
+    ido(ora, perc)
+    keses = random.randint(1,120)
     print("\nAhhoz, hogy eljuss Becsbe, eloszor el kell menj otthonrol Gyorbe, ezert Gyomoren kezdesz.")
-    print(f"\nKésik a vonatod {random.randint(1, 120)} percet. Megvárod?")
+    print(f"\nKésik a vonatod {keses} percet. Megvárod?")
     print("1 - Igen \t 2 - Nem")
     v = input("")
     match v:
         case '1':
-            gyor()
+            v = 0
+            perc += keses
+            energia -= random.randint(1,15)
+            ido(ora, perc)
+            gyor(v)
         case '2':
-            Vesztettel()
+            print("Ha nem akarsz jatszani minek nyitottad meg?")
+            exit
         case default:
             gyomore(v)
 
-def gyor():
-    os.system('cls')
+
+def gyor(v : str):
+    global energia
+    global penz
+    global ora
+    global perc
+    os.system("cls")
+    statPrinteles(energia,penz)
+    ido(ora,perc)
     print("\nHelyszin: Gyor, Vasutallomas\n1 - Iskola(Lyedlick)\n2 - Dohanybolt\n3 - Arkad\n4 - Baross ut\n6 - Becsi vonat")
+    v = input("")
+    match v:
+        case "1":
+            iskola(v)
+        case "2":
+            dohi(v)
+        case default:
+            gyor(v)
     
+def iskola(v : str):
+    global energia
+    global penz
+    global ora
+    global perc
+    os.system("cls")
+    statPrinteles(energia,penz)
+    ido(ora,perc)
+    print("\nHelyszin: Iskola(Lyedlick)")
+    print(f"\nBiztosan be akarsz menni iskolaba?\n1 - Igen  \t  2 - Nem")
+    v = input("")
+    match v:
+        case "1":
+            energia -= random.randint(1,15)
+            ora += 7
+            perc += 20
+            gyor(v)
+        case "2":
+            gyor(v)
+        case default:
+            iskola(v)
+def dohi(v : str):
+    global energia
+    global penz
+    global ora
+    global perc
+    os.system("cls")
+    statPrinteles(energia,penz)
+    ido(ora,perc)
+    print(f"\nHelyszin: Nemzeti Dohanybolt")
+    print(f"\nMivel nem vagy meg 18 eves, valakit be kell kuldened a dohiba. Mit teszel?\n1 - Bekuldok egy csovest 2 - Nem veszek cigit(-25% energia)")
+    v = input("")
+    match v:
+        case "1":
+                os.system("cls")
+                statPrinteles(energia,penz)
+                ido(ora,perc)
+                print(f"\nA csoves kihozott neked egy doboz Kek Sophiane-t")
+                print(f"\nFizetsegul adsz neki egy szalat?\n1 - Igen  \t  2 - Nem")
+                v  = input("")
+        case "2":
+            energia -= 25
+            os.system("cls")
+            statPrinteles(energia,penz)
+            ido(ora,perc)
+            print("Nagyon fogsz ma pangani.")
+            gyor(v)
--- Zsomby/main.py
+++ Zsomby/main.py
@@ -1,33 +1,9 @@
 import random, os
 from helyek import gyomore
 
-def ido(ora: int, perc: int):
-    ido = f"{ora}:{perc}"
-    if perc > 60:
-        ora += 1
-        perc = perc % 60
-    elif perc == 60:
-        ora += 1
-        perc = 0
-        ido = f"{ora}:{perc}0"
-    elif perc > 0 and perc < 10:
-        ido = f"{ora}:0{perc}"
-    elif perc == 0:
-        ido = f"{ora}:0{perc}"
-    return print(ido)
 
-def statPrinteles(energia: int, penz: int):
-    global ora
-    global perc
-    print(f"Energia:{energia}\t\tPenz: {penz}Ft", end="\t\t")
 
 def main():
-    global energia
-    global penz
-    energia = 100
-    penz = 2500
-    ora = 9
-    perc = 0
     os.system('cls')
     print("Gyor simulator 2")
     print("\n1 - Start \t 2 - Kilepes")
@@ -37,9 +13,6 @@ def main():
             os.system('cls')
             print("Udv a Gyor Simulator 2-ben, ahol ugyanaz a celod, mint az elso kiadasban, hogy megvedd a csodas vonatjegyed Becsbe. Sok szerencset!")
             input("\nENTER")
-            os.system('cls')
-            statPrinteles(energia, penz)
-            ido(ora, perc)
             v = 0
             gyomore(v)
         case '2':
@@ -47,6 +20,5 @@ def main():
         case default:
             main()
 
+
 main()
-
-
